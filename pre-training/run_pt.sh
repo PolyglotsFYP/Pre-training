e366dff720710f8dc0b820b8f60278035eec2f67
aravinda.20 <aravinda.20@cse.mrt.ac.lk>
2024-11-01 08:55:24 +0530

    Change hyper parameters

--- pre-training/run_pt.sh
+++ pre-training/run_pt.sh
@@ -12,8 +12,8 @@ tokenizer_name_or_path="polyglots/Extended-Sinhala-LLaMA"
 dataset_dir="./data"
 data_cache="./data/temp_data_cache_dir"
 per_device_train_batch_size=1
-gradient_accumulation_steps=8
-block_size=256
+gradient_accumulation_steps=4
+block_size=512
 output_dir=output_dir
 
 torchrun --nnodes 1 --nproc_per_node 4 run_clm_pt_with_peft.py \
