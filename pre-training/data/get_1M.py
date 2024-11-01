75974a78fefbdde948000f9bd2d34636c30818ae
aravinda.20 <aravinda.20@cse.mrt.ac.lk>
2024-11-01 08:25:41 +0530

    Get 1L data

--- pre-training/data/get_1M.py
+++ pre-training/data/get_1M.py
@@ -4,10 +4,10 @@ def read_and_write_first_1M_lines(input_file_path, output_file_path):
         
         # Iterate through lines and write only the first 1 million lines
         for i, line in enumerate(input_file):
-            if i >= 10**6:  # Stop after 1 million lines
+            if i >= 10**5:  # Stop after 1 million lines
                 break
             output_file.write(line)
 
 input_file_path = './filtered_output.txt'
-output_file_path = './output_1M_lines.txt'
+output_file_path = './output_1L_lines.txt'
 read_and_write_first_1M_lines(input_file_path, output_file_path)
