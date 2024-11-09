302f887db2fbce171de718cf58aa2137b5b2910d
aravinda.20 <aravinda.20@cse.mrt.ac.lk>
2024-11-09 18:52:11 +0530

    changes

--- inferencing/inferencing.py
+++ inferencing/inferencing.py
@@ -3,16 +3,13 @@ import torch
 
 # Load the model and tokenizer for inference
 model_name = "polyglots/LLaMA-Continual-Checkpoint-73456"  # Replace with the actual model name or path
-max_seq_length = 512 # Example sequence length, adjust as needed
 dtype = torch.float16  # Use float16 for efficient computation
 load_in_4bit = True  # Load model in 4-bit precision for memory efficiency
 
 # Initialize model and tokenizer
 model, tokenizer = FastLanguageModel.from_pretrained(
     model_name=model_name,
-    max_seq_length=max_seq_length,
     dtype=dtype,
-    load_in_4bit=load_in_4bit,
     resize_model_vocab = 139336
 )
 
